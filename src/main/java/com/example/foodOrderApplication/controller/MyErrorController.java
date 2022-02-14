ad17cae18183cae9ea3f16764a8584a6e1e2259f
Namrathasree <namrathasree3@gmail.com>
2022-02-14 18:43:57 +0530

    firstcommit

---
+++ src/main/java/com/example/foodOrderApplication/controller/MyErrorController.java
@@ -0,0 +1,14 @@
+package com.example.foodOrderApplication.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MyErrorController implements ErrorController {
+    @GetMapping("/error")
+    public String handleError() {
+        return "error";
+    }
+}
